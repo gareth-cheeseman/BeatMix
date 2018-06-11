be6d1a797d34ba6991619293b5c1fa42fd69b9ee
gareth-cheeseman <garethcheeseman@humbug.local>
2018-06-11 22:28:20 +0100

    Adds toggleDrum

--- public/js/script.js
+++ public/js/script.js
@@ -3,3 +3,33 @@ let kicks = [].fill(false, 0, 16);
 let snares = [].fill(false, 0, 16);
 let hiHats = [].fill(false, 0, 16);
 let rideCymbals = [].fill(false, 0, 16);
+
+const toggleDrum = (arrayName, index) => {
+  if (typeof index !== number || index > 15) {
+    return;
+  }
+
+  switch (arrayName) {
+    case 'kicks':
+      kicks[index] === false ? (kicks[index] = true) : (kicks[index] = false);
+      break;
+    case 'snares':
+      snares[index] === false
+        ? (snares[index] = true)
+        : (snares[index] = false);
+      break;
+    case 'hiHats':
+      hiHats[index] === false
+        ? (hiHats[index] = true)
+        : (hiHats[index] = false);
+      break;
+    case 'rideCymbals':
+      rideCymbals[index] === false
+        ? (rideCymbals[index] = true)
+        : (rideCymbals[index] = false);
+      break;
+    default:
+      return;
+      break;
+  }
+};
